ca27220509b66fbde648760381e546243fd1740f
임현홍 <bbnerino@gmail.com>
2025-04-08 00:28:49 +0900

    feat : startscript

--- startscript.js
+++ startscript.js
@@ -1,2 +1,13 @@
-const cmd = require("node-cmd");
-cmd.run("npm run start");
+import { exec } from "child_process";
+
+exec("npx next start", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Stdout: ${stdout}`);
+});
